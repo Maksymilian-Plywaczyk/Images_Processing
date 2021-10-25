c6c9567a2c7ecd21a0935e9b8ed0493c0a69c09e
Maksymilian Pływaczyk <plywak12331@gmail.com>
2021-10-25 14:58:38 +0200

    add text to figures

--- lab_PO_04/main.py
+++ lab_PO_04/main.py
@@ -5,22 +5,27 @@ drawing = False  # true if mouse is pressed
 mode =  True    # if True, draw rectangle.
 ix, iy = -1, -1
 
-fontFace = "FONT_HERSHEY_SCRIPT_SIMPLEX"
-
+fontFace = cv.FONT_HERSHEY_SCRIPT_SIMPLEX
+rectagle =0
+circle =0
 
 # mouse callback function
 def draw_circle(event, x, y, flags, param):
-    global ix, iy, drawing, mode
+    global ix, iy, drawing, mode,rectagle,circle
     if event == cv.EVENT_LBUTTONDBLCLK:
         drawing = False
         ix, iy = x, y
         if mode == True:
+            rectagle+=1
             cv.rectangle(img, (ix, iy), (x+100, y+100), (0, 255, 0), -1)
+            cv.putText(img,str(rectagle),(x,y),fontFace,2,(255,255,255))
     if event == cv.EVENT_RBUTTONDBLCLK:
             drawing = False
             ix, iy= x, y
+            circle +=1
             if mode == True:
                 cv.circle(img,(ix, iy),30,(0,0,255),None)
+                cv.putText(img, str(circle), (x, y), fontFace, 2, (255, 255, 255))
 
 img = np.zeros((512,512,3), np.uint8)
 cv.namedWindow('image')
