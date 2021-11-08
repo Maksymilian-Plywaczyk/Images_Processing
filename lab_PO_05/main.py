1fc89dee4ba7849663e6bb8f2359c39467e4b5b7
Maksymilian Pływaczyk <plywak12331@gmail.com>
2021-11-08 14:49:44 +0100

    Show image with Sobel mask in axis X and Y and show module of image

--- lab_PO_05/main.py
+++ lab_PO_05/main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+from time import perf_counter
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def filter2D():
+    path="baby_yoda.jpg"
+    img=cv2.imread(path,0)
+    nameWindow='Standard photo in colour grey'
+    nameWindow1='Sobel mask x'
+    nameWindow2='Sobel mask y'
+    cv2.namedWindow(nameWindow)
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)/4
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)/4
+    print('Max value of SobelX: ',np.max(sobelx))
+    print('Max value of SobelY: ',np.max(sobely))
+    np.abs(sobelx).astype(np.uint8)
+    module=np.sqrt(sobelx ** 2 + sobely ** 2)
 
+    print('Max value of module: ',np.max(module))
+    while True:
+
+        cv2.imshow(nameWindow,img)
+        cv2.imshow(nameWindow1,np.abs(sobelx).astype(np.uint8))
+        cv2.imshow(nameWindow2, np.abs(sobely).astype(np.uint8))
+        cv2.imshow('Module',module.astype(np.uint8))
+        key_code=cv2.waitKey(1)
+        if key_code==27:
+            exit()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    filter2D()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
