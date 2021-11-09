d0d94e2c06c420ec1ec6ec50b9a119804ff0d902
Maksymilian Pływaczyk <plywak12331@gmail.com>
2021-11-09 19:25:48 +0100

    Add Name to number of rectangle and circle, delete unnecessery variables

--- lab_PO_04/main.py
+++ lab_PO_04/main.py
@@ -1,39 +1,30 @@
 import cv2 as cv
 import numpy as np
 
-drawing = False  # true if mouse is pressed
-mode =  True    # if True, draw rectangle.
-ix, iy = -1, -1
-
-fontFace = cv.FONT_HERSHEY_SCRIPT_SIMPLEX
-rectagle =0
-circle =0
+rectagle = 0
+circle = 0
 # mouse callback function
-def draw_circle(event, x, y, flags, param):
-    global ix, iy, drawing, mode,rectagle,circle
-    if event == cv.EVENT_LBUTTONDBLCLK:
-        drawing = False
-        ix, iy = x, y
-        if mode == True:
+def MouseCallback(event, x, y, flags, param):
+    ix, iy = 0,0
+    fontFace = cv.FONT_HERSHEY_COMPLEX_SMALL
+    global rectagle, circle
+    if event == cv.EVENT_LBUTTONUP : #idicates that the left mouse button is released
+            ix, iy = x, y
             rectagle+=1
-            cv.rectangle(img, (ix, iy), (x+100, y+100), (0, 255, 0), -1)
-            cv.putText(img,str(rectagle),(x,y),fontFace,2,(255,255,255))
-    if event == cv.EVENT_RBUTTONDBLCLK:
-            drawing = False
+            cv.rectangle(img, (ix, iy), (x+100, y+100), (255, 0, 0), cv.FILLED) #COLOUR  BGR in OPENCV
+            cv.putText(img,"Rectangle: "+str(rectagle),(x,y),fontFace,2,(255,255,255))
+    if event == cv.EVENT_RBUTTONUP : #idicates that the right  mouse button is released
             ix, iy= x, y
             circle +=1
-            if mode == True:
-                cv.circle(img,(ix, iy),30,(0,0,255),None)
-                cv.putText(img, str(circle), (x, y), fontFace, 2, (255, 255, 255))
+            cv.circle(img,(ix, iy),30,(0,0,255),cv.FILLED)#COLOR  BGR in OpenCV
+            cv.putText(img, "Circle: "+str(circle), (x, y), fontFace, 2, (255, 255, 255))
 
-img = np.zeros((512,512,3), np.uint8)
+img = np.zeros((512, 512, 3), np.uint8) #create a black image, a window bind the function to window
 cv.namedWindow('image')
-cv.setMouseCallback('image',draw_circle)
-
-
+cv.setMouseCallback('image', MouseCallback)
 while True:
     cv.imshow('image',img)
     k = cv.waitKey(1) & 0xFF
     if k == 27:
-        break
+        exit()
 cv.destroyAllWindows()
