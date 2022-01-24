6cd8fa1cba8c366755726caa8f4a3e6a54e1de57
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-01-24 13:42:56 +0100

    Change main code for exercise

--- Detecting_fruits_final_project/detect_fruits.py
+++ Detecting_fruits_final_project/detect_fruits.py
@@ -22,60 +22,48 @@ def detect_fruits(img_path: str) -> Dict[str, int]:
     jpg = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
     # TODO: Implement detection method.
-    banana = 0
     orange = 0
     apple = 0
+    banana = 0
     jpg = cv2.resize(jpg, dsize=(1000, 800), interpolation=cv2.INTER_AREA)
     hsv_jpg = cv2.cvtColor(jpg, cv2.COLOR_BGR2HSV)
-    lower_yellow = np.array([20, 100, 160])
-    upper_yellow = np.array([30, 255, 255])
-    mask_yellow = cv2.inRange(hsv_jpg, lower_yellow, upper_yellow)
-    result_yellow = cv2.bitwise_and(jpg, jpg, mask=mask_yellow)
 
-    # finding contours
-
-    banana_contouries = cv2.findContours(mask_yellow, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    banana_contouries = banana_contouries[0]
-    for c in banana_contouries:
+    # Banana
+    lower_banana = np.array([20, 100, 160])
+    upper_banana = np.array([30, 255, 255])
+    mask_banana = cv2.inRange(hsv_jpg, lower_banana, upper_banana)
+    banana_contours = cv2.findContours(mask_banana, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    banana_contours = banana_contours[0]
+    for c in banana_contours:
         x, y, w, h = cv2.boundingRect(c)
-        print('banana width', w)
         if w > 119:
-            cv2.rectangle(jpg, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.rectangle(jpg, (x, y), (x + w, y + h), (0, 0, 255), 2)  ##BGR
             banana += 1
 
+    # Orange
     lower_orange = np.array([10, 200, 200])
     upper_orange = np.array([25, 255, 255])
     mask_orange = cv2.inRange(hsv_jpg, lower_orange, upper_orange)
-    result_orange = cv2.bitwise_and(jpg, jpg, mask=mask_orange)
-    orange_contouries = cv2.findContours(mask_orange, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    orange_contouries = orange_contouries[0]
-    for o in orange_contouries:
+    orange_contours = cv2.findContours(mask_orange, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    orange_contours = orange_contours[0]
+    for o in orange_contours:
         x, y, w, h = cv2.boundingRect(o)
-        print('orange width', w)
         if w > 64:
-            cv2.rectangle(jpg, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.rectangle(jpg, (x, y), (x + w, y + h), (255, 0, 0), 2)  ##BGR
             orange += 1
 
-    # lower boundary RED color range values; Hue (0 - 10)
-    lower1 = np.array([0, 100, 20])
-    upper1 = np.array([8, 255, 255])
+    lower2 = np.array([0, 128, 24])
+    upper2 = np.array([10, 255, 255])
 
-    # upper boundary RED color range values; Hue (160 - 180)
-    lower2 = np.array([160, 100, 20])
-    upper2 = np.array([179, 255, 255])
-
-    lower_mask = cv2.inRange(hsv_jpg, lower1, upper1)
     upper_mask = cv2.inRange(hsv_jpg, lower2, upper2)
 
-    apple_mask = lower_mask + upper_mask
-    result_apple = cv2.bitwise_and(jpg, jpg, mask=apple_mask)
+    apple_mask = upper_mask
     apple_contours = cv2.findContours(apple_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     apple_contours = apple_contours[0]
     for o in apple_contours:
         x, y, w, h = cv2.boundingRect(o)
-        print('Apple widht: ', w)
-        if w > 100:
-            cv2.rectangle(jpg, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if h > 80:
+            cv2.rectangle(jpg, (x, y), (x + w, y + h), (0, 255, 0), 2)  ##BGR
             apple += 1
 
     return {'apple': apple, 'banana': banana, 'orange': orange}
