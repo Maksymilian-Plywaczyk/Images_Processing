5f1c862c5bd6e6bffaf03c4e92b0c0b774229efc
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-01-25 14:12:19 +0100

    Workspace changes

--- Detecting_fruits_final_project/workspace.py
+++ Detecting_fruits_final_project/workspace.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 
 ##TODO poprawić implementacje problemu, aby optymalizacja byla sprawniejsza
-#TODO sprawdzić tracbara
 def empty_callback(value):
     print(f'Trackbar reporting for duty with value: {value}')
     pass
@@ -11,7 +10,7 @@ def HSV_without_trackbar():
     orange = 0
     apple =  0
     banana = 0
-    path = "data/09.jpg"
+    path = "data/01.jpg"
     jpg = cv2.imread(path,1)
     jpg = cv2.resize(jpg, dsize=(1000, 800), interpolation=cv2.INTER_AREA)
     hsv_jpg = cv2.cvtColor(jpg, cv2.COLOR_BGR2HSV)
@@ -25,7 +24,7 @@ def HSV_without_trackbar():
     for c in banana_contours:
         x,y,w,h = cv2.boundingRect(c)
         print('Banana width', w)
-        if w > 119:
+        if w > 120:
             cv2.rectangle(jpg, (x, y),(x + w, y + h), (0,0,255), 2) ##BGR
             banana += 1
 
@@ -44,22 +43,39 @@ def HSV_without_trackbar():
             orange += 1
 
 
-    lower2 = np.array([0, 128, 24])
-    upper2 = np.array([10, 255, 255])
+    lower1 = np.array([0, 38, 24])
+    upper1 = np.array([9, 255, 255])
 
-    upper_mask = cv2.inRange(hsv_jpg, lower2, upper2)
+    lower2 = np.array([12, 128, 0])
+    upper2 = np.array([12,255, 255])
 
+    upper_mask = cv2.inRange(hsv_jpg, lower1, upper1)
+    upper_mask1 = cv2.inRange(hsv_jpg, lower2, upper2)
+
+    apple_mask1 = upper_mask1
     apple_mask = upper_mask
+
     apple_contours = cv2.findContours(apple_mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     apple_contours = apple_contours[0]
+    apple_contours1 = cv2.findContours(apple_mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    apple_contours1 = apple_contours1[0]
     for o in apple_contours:
         x,y,w,h = cv2.boundingRect(o)
-        print('Apple height: ',h)
-        if h > 80:
+        #print('Apple width: ',w)
+        #print('Apple height: ',h)
+        if w > 120:
             cv2.rectangle(jpg, (x, y),(x + w, y + h), (0,255,0), 2) ##BGR
             apple += 1
+            cv2.putText(jpg, 'Width of rectangle' + str(w), (x + 10, y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
-
+    for o in apple_contours1:
+        x,y,w,h = cv2.boundingRect(o)
+        print('Apple width: ',w)
+        if h > 155:
+            cv2.rectangle(jpg, (x, y),(x + w, y + h), (0,255,0), 2) ##BGR
+            apple += 1
+            cv2.putText(jpg, 'Height of rectangle' + str(h), (x + 10, y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                        (255, 0, 0), 2)
     print('Number of orange: '+str(orange))
     print('Number of banana: '+str(banana))
     print('Number of apple: '+str(apple))
@@ -71,7 +87,7 @@ def HSV_without_trackbar():
             exit()
 
 def HSV_with_trackbar():
-    path = "data/00.jpg"
+    path = "data/05.jpg"
     nameWindow = "Trackbar HSV"
     img = cv2.imread(path, 1)
     resize_img = cv2.resize(img, dsize=(1000, 800), interpolation=cv2.INTER_AREA)
